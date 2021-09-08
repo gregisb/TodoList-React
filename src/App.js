3e48b7454302b6e35dd33c854845234b01d2727e
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-09-07 22:46:39 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import TodoList from './components/TodoList'
+
+
+function App() {
+  return (
+    <div className="todo-app">
+        <TodoList />
+        
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message() {
+    return (
+        <div className='message'>
+            <p>Clique na tarefa para marcar como completa!</p>
+        </div>
+    )
+}
+
+export default Message
